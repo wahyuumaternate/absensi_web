23c8e8c99a5e83d2e85fbc376cdc3d62ab450e5e
wahyuumaternate <retmujago@gmail.com>
2024-11-28 04:34:15 +0900

    seeder absen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Absensi;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -39,6 +40,19 @@ class DatabaseSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+        ]);
+
+        Absensi::insert([
+            [
+                "id" => 3,
+                "user_id" => 1,
+                "tanggal" => "2024-11-04",
+                "jam_masuk" => "08:12:06",
+                "jam_keluar" => null,
+                "status" => "hadir",
+                "created_at" => now(), // menggunakan waktu saat ini
+                "updated_at" => null, // atau bisa menggunakan now() jika diperlukan
+            ]
         ]);
     }
 }
